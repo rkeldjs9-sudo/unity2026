ab9352e91e8d2032be6c88dd9dce2120b401edda
infj294 <rkeldjs9@gmail.com>
2026-04-27 12:59:24 +0900

    04-27

--- CH06/Assets/20.Scripts/PlayerController.cs
+++ CH06/Assets/20.Scripts/PlayerController.cs
@@ -1,50 +1,41 @@
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     float jumpForce = 300f;
-    float walkForce = 0f;
-    float maxWalkSpeed = 0f;
-
-    public Sprite[] walkSprites;
-    public float animationPeriod = 0.1f;
-    float time = 0;
-    int idx = 0;
-    SpriteRenderer sr;
-
+    public float walkForce = 7f;
+    float maxWalkSpeed = 1f;
     Rigidbody2D rb;
+    Animator anim;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Application.targetFrameRate = 60;
         rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             rb.AddForce(transform.up * jumpForce);
         }
-
-        if(rb.linearVelocityX < maxWalkSpeed)
+        if (rb.linearVelocityX < maxWalkSpeed)
         {
             rb.AddForce(transform.right * walkForce);
         }
-
-        time += Time.deltaTime;
-        if (time > animationPeriod)
+        if (rb.linearVelocityY != 0)
         {
-            time = 0;
-            sr.sprite = walkSprites[idx];
-            idx++;
-            if(idx == walkSprites.Length)
-            {
-                idx = 0;
-            }
+            anim.SetBool("isJumping", true);
         }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("¼º°ø");
     }
 }
