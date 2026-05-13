0eec8983f81b79107ae7ef970982b417fd7c8f0c
infj294 <rkeldjs9@gmail.com>
2026-05-13 16:30:46 +0900

    05-13

--- CH07/Assets/20.Scripts/BamsongiGenerator.cs
+++ CH07/Assets/20.Scripts/BamsongiGenerator.cs
@@ -1,9 +1,11 @@
+using Unity.Hierarchy;
 using UnityEngine;
 
 public class BamsongiGenerator : MonoBehaviour
 {
     public GameObject bamsongiPrefab;
     public float throwForce = 10f;
+    public float minPower = 200f;
 
     float startY;
 
@@ -16,10 +18,13 @@ public class BamsongiGenerator : MonoBehaviour
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            GameObject bamsoni = Instantiate(bamsongiPrefab);
-            bamsoni.transform.position = transform.position;
+
 
             float power = Input.mousePosition.y - startY;
+            if (power < minPower) return;
+
+            GameObject bamsoni = Instantiate(bamsongiPrefab);
+            bamsoni.transform.position = transform.position;
 
             Vector3 dir = transform.forward + transform.up * 0.5f;
             bamsoni.GetComponent<Bamsongicontroller>().Shoot(dir * power * throwForce);
